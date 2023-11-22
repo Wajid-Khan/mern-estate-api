020fa0280a00e9fa4f5cf0010900c6d30e7bb961
Wajid Khan <145491250+securemax-mabaat@users.noreply.github.com>
2023-11-22 11:04:16 +0530

    create a middle to handle possible error

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ app.use("/api/user", userRouter);
 
 //sign up appi
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    }); 
+});
