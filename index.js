20ce1e94af9cf55bc5f60d38e2adf8c3fcbe21e4
Wajid Khan <wajid.developer1@gmail.com>
2023-11-24 02:02:08 +0530

    create sign in api route

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from 'cors';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO);
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
 });
 
 //api 
